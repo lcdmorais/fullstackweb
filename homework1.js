44f7af16ee4869cb9567e6967923802be3598a2b
leonardomorais <leumorais@live.com>
2023-05-01 15:46:19 -0300

    firstcommit

---
+++ homework1.js
@@ -0,0 +1,19 @@
+let alunos = ['mila', 'juninho', 'mauro', 'leo', 'anderson', 'fernando', 'neia', 'murilo', 'matheus', lari];
+let serie = [1, 2, 3];
+let nota01 = [2, 3, 4, 8, 5, 4, 3, 8, 9, 10];
+let nota02 = [9, 3, 7, 8, 9, 8, 2, 9, 5, 3];
+
+let alunos = [{'name':"mila", 'serie':1, 'nota01':10, 'nota02':2}, {'name':'juninho','serie':2, 'nota01':10, 'nota02':6},{'name':'leo', 'serie': 3, 'nota01':10,'nota02':10}]
+
+for (let i =0; i < alunos.length; i++)
+for (let s = 0 s < serie.length; s++)
+
+let media = (nota01[i] + nota02[i]) / 2;
+
+if (media >= 5) { 
+    console.log (alunos[i] + serie[s] + 'APROVADO')
+} else {
+    console.log (alunos[i] + serie[s] +'reprovado');
+}
+
+
